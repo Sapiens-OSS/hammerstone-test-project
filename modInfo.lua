8a8b93a3cb816e8bce4fa655ada03038473bc0e9
SirLich <lrkoehler77@gmail.com>
2022-12-14 23:27:19 +0100

    Push initial version of the test project

---
+++ modInfo.lua
@@ -0,0 +1,9 @@
+local modInfo = {
+  name = "Hammerstone Test Mod",
+  description = "A test mod for Hammerstone",
+  version = "0.0.0",
+  type = "world",
+  developer = "SirLich",
+  loadOrder = 1,
+}
+return modInfo
